69f55fbc3905b0ae8889fac952afb7c9153f8d36
yizhichangyuan <49146565+yizhichangyuan@users.noreply.github.com>
2026-02-22 01:22:54 +0800

    Simplify App - remove Router for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 }
